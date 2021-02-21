de13c44568efa9bf9c63545a5dc0c3be4286c5ea
sahil9001 <sahilsilare@gmail.com>
2021-02-21 13:16:30 +0530

    feat:linted code

--- tests/test_app_config.py
+++ tests/test_app_config.py
@@ -1,12 +1,15 @@
 import unittest
-import os
+
 from django.test import TestCase
+
 from main import settings
+
 
 class TestConfig(TestCase):
     def test_app_testing_config(self):
         self.assertIsNotNone(settings.SECRET_KEY)
-        self.assertNotEqual(settings.SECRET_KEY,'')
- 
+        self.assertNotEqual(settings.SECRET_KEY, "")
+
+
 if __name__ == "__main__":
     unittest.main()
