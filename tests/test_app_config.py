259dc70e79fa9d28b26da4dd6ca3fe8e904924da
sahil9001 <sahilsilare@gmail.com>
2021-02-22 10:15:14 +0530

    changed required suggessions

--- tests/test_app_config.py
+++ tests/test_app_config.py
@@ -1,15 +1,8 @@
-import unittest
-
+from django.conf import settings
 from django.test import TestCase
-
-from main import settings
 
 
 class TestConfig(TestCase):
-    def test_app_testing_config(self):
+    def test_config(self):
         self.assertIsNotNone(settings.SECRET_KEY)
         self.assertNotEqual(settings.SECRET_KEY, "")
-
-
-if __name__ == "__main__":
-    unittest.main()
