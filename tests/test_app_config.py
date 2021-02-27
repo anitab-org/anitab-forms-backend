9b42ca150180998828333f0f7c973fd1eec706ff
sahil9001 <sahilsilare@gmail.com>
2021-02-27 12:48:12 +0530

    Modified github workflows

--- tests/test_app_config.py
+++
@@ -1,8 +0,0 @@
-from django.conf import settings
-from django.test import TestCase
-
-
-class TestConfig(TestCase):
-    def test_config(self):
-        self.assertIsNotNone(settings.SECRET_KEY)
-        self.assertNotEqual(settings.SECRET_KEY, "")
